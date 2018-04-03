60555f6dadc2b13098639058941584a6ae43b39a
jose-guerrero <jose.guerrerog@outlook.com>
2018-04-04 00:02:51 +1000

    adding welcome view

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -16,9 +16,11 @@ namespace MvcMovie.Controllers
         // GET: /HelloWorld/Welcome/ 
         // url -> http://localhost:5000/HelloWorld/Welcome?name=Rick&id=4
         // url -> http://localhost:5000/HelloWorld/Welcome/4?name=Rick
-        public string Welcome(string name,int ID=1)
+        public IActionResult Welcome(string name,int numTimes=1)
         {
-            return HtmlEncoder.Default.Encode($"Hello {name}, ID: {ID}");
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
